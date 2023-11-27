0fd29f65e86bf86e6fd6b531e04c7866a5b5f4ec
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2023-11-27 09:45:54 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,36 +2,36 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber =  "919931122319"
-global.ownername = "Ayush Pandey"
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber =  "6281511073357"
+global.ownername = "BAYU RIZKI ZULFIKRI"
+global.ytname = "YT: LASUTTEAM_OFFICE"
+global.socialm = "GitHub: cacacr4ck"
+global.location = "Indonesia"
 
-global.ownername = 'Ayush' //owner name
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.ownername = 'BAYU RIZKI ZULFIKRI' //owner name
+global.botname = 'ZF-Assistant' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎✘ZF-Assistant✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'ZF-Assistant'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "ZF-Assistant."
 
 //theme link
-global.link = 'https://chat.whatsapp.com/Jllsl2OaQNoBjepxzuVsZM'
+global.link = ''
 
 //custom prefix
 global.prefa = ['.']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
-global.anti212 = true //auto block +212
+global.anti212 = false //auto block +212
 global.autoread_status = false //auto view status/story
 
 
